4672c8196fbf2b7a80647aa884e05cd71ad0c91f
tashenoff <kail.dead.kd@gmail.com>
2025-05-13 21:31:54 +0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Отключаем использование нативных модулей Rollup
+    rollupOptions: {
+      context: 'globalThis',
+    },
+  },
 })
